620769633e3e3f4d8f37a7d32c3e5bc60c37092e
pyush-18 <piyushjoshi1812@gmail.com>
2025-02-21 14:52:05 +0530

    updating

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,10 +23,10 @@ app.use("/api/messages", messageRoute);
 
 import {} from "../../frontend/dist"
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 export { app };
