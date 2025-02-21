301797ecfaaf178e8fae7875b122c0766c631672
pyush-18 <piyushjoshi1812@gmail.com>
2025-02-21 14:48:57 +0530

    updating

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,32 +1,32 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import path from "path"
-const app = express()
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true
-}
-app.use(cors(corsOptions))
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-import authRoute from "../routes/auth.route.js"
-import messageRoute from "../routes/message.route.js"
+import authRoute from "../routes/auth.route.js";
+import messageRoute from "../routes/message.route.js";
 
-app.use("/api/auth",authRoute)
-app.use("/api/messages",messageRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/messages", messageRoute);
 
-if(process.env.NODE_ENV === "development"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-}
+import {} from "../../frontend/dist"
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-})
+  res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+});
 
-export {app}
+export { app };
