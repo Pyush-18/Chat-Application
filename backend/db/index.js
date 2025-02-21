0cfee3251b50ac8fbcebd02f9070400792d0fff3
pyush-18 <piyushjoshi1812@gmail.com>
2025-02-21 14:20:58 +0530

    mongodb string update

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async() => {
     try {
-        const connectInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
+        const connectInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`Mongodb connected DB host !! ${connectInstance.connection.host}`)
     } catch (error) {
         console.log(`Mongodb disconnected ${error?.message}`)     
